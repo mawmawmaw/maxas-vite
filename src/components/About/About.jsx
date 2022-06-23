45acb2ba4c2aff62ed98c71169866cf588dbe5ca
Mauricio <shinji.256@gmail.com>
2022-06-23 17:28:42 -0500

    Added images in landing and added fix for main landing animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,14 @@
 import './About.css';
+import doodle from './doodle.svg';
 const About = () => {
     return (
         <div id="about" className='section'>
             <div className='container'>
                 <h2 className='section-title'>About</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra interdum vestibulum. Etiam condimentum elementum augue quis lobortis. Sed imperdiet congue libero, non elementum orci condimentum non. Duis eget urna tempor elit tincidunt porta. Integer vestibulum mauris nunc, eget accumsan sem fringilla nec. Cras fermentum, risus euismod elementum vulputate, nisi felis cursus odio, non lacinia lacus dolor quis nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla at porta est. Ut nunc mauris, fringilla vitae tortor non, placerat imperdiet tellus. Suspendisse ante nibh, vehicula ut finibus eu, aliquet a lorem. Proin maximus orci sit amet est cursus, at aliquam sem pretium. Cras vel odio lacinia, egestas sapien ut, egestas est. Nullam ornare dictum erat quis fringilla.</p>
+                <div className='content grid left'>
+                    <img src={doodle} alt="doodle" width="300"/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra interdum vestibulum. Etiam condimentum elementum augue quis lobortis. Sed imperdiet congue libero, non elementum orci condimentum non. Duis eget urna tempor elit tincidunt porta. Integer vestibulum mauris nunc, eget accumsan sem fringilla nec. Cras fermentum, risus euismod elementum vulputate, nisi felis cursus odio, non lacinia lacus dolor quis nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla at porta est. Ut nunc mauris, fringilla vitae tortor non, placerat imperdiet tellus. Suspendisse ante nibh, vehicula ut finibus eu, aliquet a lorem. Proin maximus orci sit amet est cursus, at aliquam sem pretium. Cras vel odio lacinia, egestas sapien ut, egestas est. Nullam ornare dictum erat quis fringilla.</p>
+                </div>
             </div>
         </div>
     )
--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -1,10 +1,14 @@
 import './Context.css';
+import doodle from './doodle.svg';
 const Context = () => {
     return (
         <div id="context" className='section'>
             <div className='container'>
                 <h2 className='section-title'>Context</h2>
-                <p>Nullam in varius felis. Duis sit amet placerat felis, sit amet ullamcorper libero. Proin elementum nisl vitae est tristique, a vulputate risus pharetra. Cras lacinia lacus eget arcu consectetur dictum. Phasellus mollis rutrum ligula, ut tristique massa vehicula vel. Cras dictum erat suscipit tincidunt efficitur. Sed interdum porttitor magna, nec ultrices turpis tincidunt eu. Ut vitae accumsan mi. Duis nulla enim, fringilla at accumsan quis, mattis at velit. Vestibulum pulvinar velit vitae arcu venenatis congue. Mauris molestie ligula id posuere mollis. Morbi sit amet ipsum urna.</p>
+                <div className='content grid left'>
+                    <img src={doodle} alt="doodle" width="300"/>
+                    <p>Nullam in varius felis. Duis sit amet placerat felis, sit amet ullamcorper libero. Proin elementum nisl vitae est tristique, a vulputate risus pharetra. Cras lacinia lacus eget arcu consectetur dictum. Phasellus mollis rutrum ligula, ut tristique massa vehicula vel. Cras dictum erat suscipit tincidunt efficitur. Sed interdum porttitor magna, nec ultrices turpis tincidunt eu. Ut vitae accumsan mi. Duis nulla enim, fringilla at accumsan quis, mattis at velit. Vestibulum pulvinar velit vitae arcu venenatis congue. Mauris molestie ligula id posuere mollis. Morbi sit amet ipsum urna.</p>
+                </div>
             </div>
         </div>
     )
